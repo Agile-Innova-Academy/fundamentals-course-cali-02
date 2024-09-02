fdde8f72c776cae7578cae8ce8c21b152d2cbfe2
yudithleon <leonkyudith@gmail.com>
2024-09-02 16:38:29 -0500

    ✨ feat(Webpack y babel):

---
+++ sesion_5/5.1-Axios/Ejercicio/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./scripts/scripts.js",
+  output: {
+    path: path.resolve(__dirname, "dist"), //carpeta de salida
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // archivos que tipo .js
+        use: { loader: "babel-loader" },
+        exclude: /node_modules/, //No va usar los archivos que estendentro de node_modules
+      },
+      {
+        test: /\.json$/, // archivos que tipo .json
+        type: "json",
+      },
+    ], // use babel para transpilar
+  },
+  mode: "production", //Modo de desarrollo ó 'production'
+};
---
+++ sesion_6/6.4-Ejemplo/src/index.js
@@ -0,0 +1,6 @@
+import { num } from "./urls.js"
+
+console.log("Hola")
+
+console.log("Desde productions", num)
+
